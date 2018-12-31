96ea966fc16ee0837a3ca984c536105c46e4207d
Keith Bloom <keith.bloom@gmail.com>
2018-12-31 15:51:20 +0000

    fixes storybook

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,5 +1,6 @@
 // automatically import all files ending in *.stories.tsx
-const req = require.context('../_stories', true, /.stories.tsx$/);
+import { configure } from '@storybook/react';
+const req = require.context('../src', true, /.stories.tsx$/);
 
 function loadStories() {
   req.keys().forEach(req);
