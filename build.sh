38107d99d18e1deb0bc52cfd08265947d83cd352
Keith Bloom <keithbloom@users.noreply.github.com>
2019-03-29 14:21:05 +0000

    build tag and push docker images (#31)

--- build.sh
+++ build.sh
@@ -1,9 +1,40 @@
-#! /bin/bash
+#! /usr/bin/env bash
+set -e
 
-cd ./server
-docker build . --target build
-cd ..
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-cd ./client
-docker build .
-cd ..
+if [ $# -gt 1 -a "$1" == "push" ]
+then 
+    TAG=$2
+
+    echo "Pushing TardisBank ($TAG)"
+    echo "-------------------------"
+
+    docker push tardisbank/server:$TAG
+    docker push tardisbank/client:$TAG
+    docker push tardisbank/nginx:$TAG
+
+elif [ $# -gt 1 -a "$1" == "tag" ] 
+then
+    TAG=$2
+
+    echo "Tagging TardisBank ($TAG)"
+    echo "-------------------------"
+
+    docker tag tardisbank/server tardisbank/server:$TAG
+    docker tag tardisbank/client tardisbank/client:$TAG
+    docker tag tardisbank/nginx tardisbank/nginx:$TAG
+
+else
+    echo "Building TardisBank"
+    echo "-------------------"
+
+    chmod u+x $DIR/server/build.sh
+    $DIR/server/build.sh
+ 
+    chmod u+x $DIR/client/build.sh
+    $DIR/client/build.sh
+
+    chmod u+x $DIR/nginx/build.sh
+    $DIR/nginx/build.sh
+fi
