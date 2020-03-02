5c13a4c4c923980f8a475b90320d1d4b965434dc
Keith Bloom <keith.bloom@gmail.com>
2020-03-02 09:34:56 +0000

    adds buld_version as env var

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -13,6 +13,7 @@ DEPLOY_FOLDER="~/deployment/${COMPOSE_PROJECT_NAME}"
 
 run "mkdir -p $DEPLOY_FOLDER" 
 scp docker-compose.yml docker-compose.prod.yml ./devops/.env $REMOTE_USER@$REMOTE_HOST:$DEPLOY_FOLDER
-run "cd $DEPLOY_FOLDER && docker-compose -f docker-compose.prod.yml -f docker-compose.yml down"
-run "cd $DEPLOY_FOLDER && docker-compose -f docker-compose.prod.yml -f docker-compose.yml pull"
-run "cd $DEPLOY_FOLDER && docker-compose -f docker-compose.prod.yml -f docker-compose.yml up -d"
+run "cd $DEPLOY_FOLDER && docker system prune -f"
+run "export BUILD_VERSION=$BUILD_VERSION && export DOCKER_USER=$DOCKER_USER && cd $DEPLOY_FOLDER && docker-compose -f docker-compose.prod.yml -f docker-compose.yml down"
+run "export BUILD_VERSION=$BUILD_VERSION && export DOCKER_USER=$DOCKER_USER && cd $DEPLOY_FOLDER && docker-compose -f docker-compose.prod.yml -f docker-compose.yml pull"
+run "export BUILD_VERSION=$BUILD_VERSION && export DOCKER_USER=$DOCKER_USER && cd $DEPLOY_FOLDER && docker-compose -f docker-compose.prod.yml -f docker-compose.yml up -d"
